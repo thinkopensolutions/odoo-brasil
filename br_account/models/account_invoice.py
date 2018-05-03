bdf0d236c0e55a7e2225a025b24daece5239e6ec
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2018-05-03 18:34:33 +0000

    Do not change account move line amount

--- br_account/models/account_invoice.py
+++ br_account/models/account_invoice.py
@@ -303,43 +303,43 @@ class AccountInvoice(models.Model):
                               state in order to be cancelled."))
         return self.action_cancel()
 
-    @api.model
-    def invoice_line_move_line_get(self):
-        res = super(AccountInvoice, self).invoice_line_move_line_get()
-
-        contador = 0
-
-        for line in self.invoice_line_ids:
-            if line.quantity == 0:
-                continue
-            res[contador]['price'] = line.price_total
-
-            price = line.price_unit * (1 - (
-                line.discount or 0.0) / 100.0)
-
-            ctx = line._prepare_tax_context()
-            tax_ids = line.invoice_line_tax_ids.with_context(**ctx)
-
-            taxes_dict = tax_ids.compute_all(
-                price, self.currency_id, line.quantity,
-                product=line.product_id, partner=self.partner_id)
-
-            for tax in line.invoice_line_tax_ids:
-                try:
-                    tax_dict = next(
-                        x for x in taxes_dict['taxes'] if x['id'] == tax.id)
-                except StopIteration:
-                    continue
-                if not tax.price_include and tax.account_id:
-                    res[contador]['price'] += tax_dict['amount']
-                if tax.price_include and (not tax.account_id or
-                                          not tax.deduced_account_id):
-                    if tax_dict['amount'] > 0.0:  # Negativo é retido
-                        res[contador]['price'] -= tax_dict['amount']
-
-            contador += 1
-
-        return res
+    # @api.model
+    # def invoice_line_move_line_get(self):
+    #     res = super(AccountInvoice, self).invoice_line_move_line_get()
+    #
+    #     contador = 0
+    #
+    #     for line in self.invoice_line_ids:
+    #         if line.quantity == 0:
+    #             continue
+    #         res[contador]['price'] = line.price_total
+    #
+    #         price = line.price_unit * (1 - (
+    #             line.discount or 0.0) / 100.0)
+    #
+    #         ctx = line._prepare_tax_context()
+    #         tax_ids = line.invoice_line_tax_ids.with_context(**ctx)
+    #
+    #         taxes_dict = tax_ids.compute_all(
+    #             price, self.currency_id, line.quantity,
+    #             product=line.product_id, partner=self.partner_id)
+    #
+    #         for tax in line.invoice_line_tax_ids:
+    #             try:
+    #                 tax_dict = next(
+    #                     x for x in taxes_dict['taxes'] if x['id'] == tax.id)
+    #             except StopIteration:
+    #                 continue
+    #             if not tax.price_include and tax.account_id:
+    #                 res[contador]['price'] += tax_dict['amount']
+    #             if tax.price_include and (not tax.account_id or
+    #                                       not tax.deduced_account_id):
+    #                 if tax_dict['amount'] > 0.0:  # Negativo é retido
+    #                     res[contador]['price'] -= tax_dict['amount']
+    #
+    #         contador += 1
+    #
+    #     return res
 
     @api.multi
     def finalize_invoice_move_lines(self, move_lines):
